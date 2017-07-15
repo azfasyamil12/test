ac9111ecb35d4610b6c7313373b717653ed85cfe
azfasyamil12 <azfasyamil12@gmail.com>
2017-07-15 04:52:14 -0700

    Add files via upload

---
+++ bot2.php
@@ -0,0 +1,15 @@
+<?php
+$date = new DateTime('2017-7-15', new DateTimeZone('Asia/Jakarta'));
+echo $date->format('Y-m-d H:i:sP') . "\n";
+
+$date->setTimezone(new DateTimeZone('Asia/Jakarta'));
+echo $date->format('Y-m-d H:i:sP') . "\n";
+?>
+
+<?php
+$t=time();
+echo($t . "<br>");
+echo(date("Y-m-d",$t));
+?>
+$channelaccesstoken =ZPD643olnk5x6/YFFmAwBznwJNvzA5sg6d2o3BOdYY+11OWJWOiMwxmSuubymlUYFmKXZqa2TFSxjoBzBH2Zoq6yBEyLQV17jkWs8dz09qnCF719RoaEa5sOGMYTvfmz7OS2kMfApIBXwzEegaPM1gdB04t89/1O/w1cDnyilFU=
+$channelsecret =a0dbf1b13f6bf03a4e85f5e0489e3102
